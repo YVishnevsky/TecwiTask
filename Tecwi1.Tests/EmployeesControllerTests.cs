0c3f239003ab4aa56531df79dae63ce22b2163d4
Yaroslav Vishnevsky <rick.ua@gmail.com>
2018-04-22 20:46:41 +0300

    added test project and simple test

---
+++ Tecwi1.Tests/EmployeesControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tecwi1.Controllers;
+using Tecwi1.Repositories;
+using Xunit;
+
+namespace Tecwi1.Tests
+{
+    public class EmployeesControllerTests
+    {
+        [Fact]
+        public async Task Delete_Employee()
+        {
+            var mockRepository = new Mock<IEmployeeRepository>();
+            var controller = new EmployeesController(mockRepository.Object);
+            int id = 99;
+            await controller.Delete(id);
+            mockRepository.Verify(repo => repo.DeleteAsync(id), Times.Once);
+        }
+    }
+}
